f76b93ef996ddedca5b7e893b779748b9c4e963a
Hyuk <79625509+testerhyuk@users.noreply.github.com>
2023-08-24 18:05:45 +0900

    약국 데이터 셋업

--- src/main/java/com/pharmacy_recommendation/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/pharmacy_recommendation/pharmacy/service/PharmacyRepositoryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -25,5 +26,10 @@ public class PharmacyRepositoryService {
         }
 
         entity.changePharmacyAddress(address);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
     }
 }
