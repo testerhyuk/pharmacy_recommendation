ccc560e9d9c88e8ef68f9ab3a54d6631a86da40e
Hyuk <79625509+testerhyuk@users.noreply.github.com>
2023-08-25 15:22:04 +0900

    Spring Retry 구현 및 검증

---
+++ src/main/java/com/pharmacy_recommendation/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package com.pharmacy_recommendation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+}
